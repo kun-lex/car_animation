cac0cefe3cdd99e596dbd3451290a8f58d7fe1f6
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-07-02 09:42:32 +0100

    update react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import Swiper from 'swiper';
+import { gsap, Power4 } from 'gsap';
+import './App.css'
 
-function App() {
+const MyComponent = () => {
+  useEffect(() => {
+    const page = document.getElementById('page');
+    const loading = document.getElementById('loading');
+    const slider = document.querySelector('.swiper');
+    const inner1 = document.getElementById('inner-1');
+    const inner2 = document.getElementById('inner-2');
+    const inner3 = document.getElementById('inner-3');
+    const car = document.querySelector('model-viewer');
+    const slideToButtons = document.querySelectorAll('[data-slide-to]');
+    const colorButtons = document.querySelectorAll('[data-color]');
+    const title = document.querySelectorAll('.title');
+    const bgImage = document.querySelector('picture');
+
+    const innerAnimationActive = {
+      duration: 1,
+      delay: 0.5,
+      ease: Power4.easeOut,
+      autoAlpha: 1,
+      yPercent: 0,
+    };
+    const innerAnimationHidden = {
+      duration: 1,
+      ease: Power4.easeOut,
+      autoAlpha: 0,
+      yPercent: -20,
+    };
+
+    const swiper = new Swiper(slider, {
+      // Swiper options
+    });
+
+    car.addEventListener('load', (event) => {
+      // Car load event handling
+    });
+
+    // Additional code...
+
+    // Return a cleanup function if necessary
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* JSX structure */}
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
